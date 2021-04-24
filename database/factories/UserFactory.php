2ec9fa0195e376791f96bec17edbe6b64aa6f8ab
ariel <francoariel01@gmail.com>
2021-04-24 23:51:54 +0200

    abril 24 23:51

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Post;
+use App\Models\CategoryPost;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -26,22 +29,103 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('123456'),
             'remember_token' => Str::random(10),
         ];
     }
+}
+
+/*
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     *//*
+    protected $model = Post::class;
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Define the model's default state.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
+     * @return array
+     *//*
+    public function definition()
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
+        $title =  $this->faker->unique()->sentence;
+        $isPublished = ['1', '0'];
+        return [
+            'user_id' => rand(1, 5),
+        'title' => $title,
+        'slug' => str_slug($title),
+        'sub_title' =>  $this->faker->sentence,
+        'details' =>  $this->faker->paragraph,
+        'post_type' => 'post',
+        'is_published' => $isPublished[rand(0, 1)],
+        'created_at' => now(),
+        'updated_at' => now(),
+        ];
     }
 }
+
+class CategoryPostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     *//*
+    protected $model = CategoryPost::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     *//*
+    public function definition()
+    {
+        return [
+        'category_id' => rand(1, 5),
+        'post_id' => rand(1, 100),
+        'created_at' => now(),
+        'updated_at' => now(),
+        ];
+    }
+}
+
+
+$factory->define(App\Models\User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => bcrypt('123456'),
+        'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(App\Models\Post::class, function (Faker $faker) {
+    $title = $faker->unique()->sentence;
+    $isPublished = ['1', '0'];
+    return [
+        'user_id' => rand(1, 5),
+        'title' => $title,
+        'slug' => str_slug($title),
+        'sub_title' => $faker->sentence,
+        'details' => $faker->paragraph,
+        'post_type' => 'post',
+        'is_published' => $isPublished[rand(0, 1)],
+        'created_at' => now(),
+        'updated_at' => now(),
+    ];
+});
+
+$factory->define(App\Models\CategoryPost::class, function (Faker $faker) {
+    return [
+        'category_id' => rand(1, 5),
+        'post_id' => rand(1, 100),
+        'created_at' => now(),
+        'updated_at' => now(),
+    ];
+});
+*/
